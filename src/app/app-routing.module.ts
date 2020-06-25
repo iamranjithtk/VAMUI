e244d2602c57bd7d668fbb4b25c54aabed230adf
Ranjith0t0k <32890982+Ranjith0t0k@users.noreply.github.com>
2020-06-25 11:37:33 +0530

    teacher functions ui updates
    teacher dashboard components updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { TeacherUploadVideoComponent } from './teacher-upload-video/teacher-upload-video.component';
+import { TeacherStudentListComponent } from './teacher-student-list/teacher-student-list.component';
+import { TeacherNotesComponent } from './teacher-notes/teacher-notes.component';
+import { TeacherNavbarComponent } from './teacher-navbar/teacher-navbar.component';
 import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { HelpComponent } from './help/help.component';
@@ -10,13 +14,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
+import { TeacherLiveClassComponent } from './teacher-live-class/teacher-live-class.component';
+import { TeacherHomeWorkComponent } from './teacher-home-work/teacher-home-work.component';
 
 const routes: Routes = [
   {
     path: 'wrapper', component: WrapperComponent, children: [
       { path: 'teachertimetable', component: TeachertimetableComponent },
       { path: 'teacherdashboard', component: TeacherDashboardComponent },
-      { path: 'admin', component: AdminComponent }
+      { path: 'teachernavbar', component: TeacherNavbarComponent },
+      { path: 'teachernotes', component: TeacherNotesComponent },
+      { path: 'teacherstudentlist', component: TeacherStudentListComponent },
+      { path: 'teacheruploadvideo', component: TeacherUploadVideoComponent },
+      { path: 'teacherliveclass', component: TeacherLiveClassComponent },
+      { path: 'teacherhomework', component: TeacherHomeWorkComponent },
     ]
   },
   { path: 'register', component: RegisterComponent },
